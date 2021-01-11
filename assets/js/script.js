3c76b430360db55a120b748069e20f307557730a
Nick Vandenberge <nvandenberge14@gmail.com>
2021-01-11 13:26:50 -0500

    function to retrieve and build current weather conditions section

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,5 +38,44 @@ $("#citySearchBtn").on("click", function (event) {
       $recentCityDiv.text(city);
       $(".recentCitiesList").append($recentCityDiv);
     });
+    weatherStats(response);
   });
 });
+
+function weatherStats(city) {
+  // Adding weather stats to object
+  let $cityInfo = {
+    name: city.name,
+    temp: `${Math.round(city.main.temp)}°F`,
+    humidity: `${city.main.humidity}%`,
+    wind: `${Math.round(city.wind.speed)} mph`,
+    conditionsIcon: `https:///openweathermap.org/img/w/${city.weather[0].icon}.png`,
+  };
+
+  // Adding an img element and src attribute to get weather icon
+  let $weatherIcon = $("<img>").attr("src", $cityInfo.conditionsIcon);
+
+  // Add text to div elements in
+  $(".weatherStats").addClass('weatherStatsStyle');
+  $("#cityHeader").text(`${$cityInfo.name} ${$currentDate}`);
+  $("#cityHeader").append($weatherIcon);
+  $("#cityTemp").text(`Temperature:  ${$cityInfo.temp}`);
+  $("#cityHumidity").text(`Humidity: ${$cityInfo.humidity}`);
+  $("#cityWind").text(`Wind Speed: ${$cityInfo.wind}`);
+
+  // Request to get UV Index value using longitude and latitiude
+  let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${APIkey}&units=imperial`;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // Adding uvIndex to cityInfo object
+    $cityInfo.uvIndex = response.value;
+
+    // Adding UV index to page
+    let $indexNumSpan = $("<span id='indexNumber'>");
+    $indexNumSpan.text($cityInfo.uvIndex);
+    $("#cityUvIndex").text("UV Index: ");
+    $("#cityUvIndex").append($indexNumSpan);
+  });
+}
